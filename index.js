953825301a03540ebcde8a24a2d4792141a90c2a
yanlinchengrui <yanlinchengrui@hotmail.com>
2019-06-26 19:36:49 +0000

    added inheritance

--- index.js
+++ index.js
@@ -1,29 +1,83 @@
 let balance = 500.00;
 
-class Withdrawal {
+class Transaction {
 
-  constructor(amount) {
-    this.amount = amount;
+  constructor(amount, account) {
+    this.amount  = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    if(this.isAllowed()) {
+      this.time = new Date();
+      this.account.addTransaction(this);
+      console.log('Yea!');
+    }
+    else {
+      console.log('Nope!');
+    }
   }
 
 }
 
+class Withdrawal extends Transaction {
 
+  get value() {
+    return -this.amount;
+  }
 
+  isAllowed() {
+    return this.account.balance < this.amount ? false : true;
+  }
+
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+
+  isAllowed() {
+    return true;
+  }
+
+}
+
+class Account {
+
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
+
+  get balance() {
+    // Calculate the balance using the transaction objects.
+    let rez = 0;
+    this.transactions.forEach((transaction) => rez += transaction.value);
+    return rez;
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+
+}
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account('billybob');
+
+console.log('Starting Balance:', myAccount.balance);
+
+const t1 = new Deposit(120.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+const t2 = new Withdrawal(110.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+const t3 = new Withdrawal(20.00, myAccount);
+t3.commit();
+
+console.log('Ending Balance:', myAccount.balance);
